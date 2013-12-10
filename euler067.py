59049b9f87520cc3c5695c471b4ac881f51e6795
proposicion47 <proposicion47@gmail.com>
2013-12-10 11:32:21 -0600

    maxima suma de camino en triangulo

---
+++ euler067.py
@@ -0,0 +1,16 @@
+import os
+print os.getcwd()
+entrada = open("triangle.txt","r")
+D=[]
+for l in entrada:
+	D.append(map(int,l.split()))
+
+nrows= len(D)
+
+for r in range(nrows-2,-1,-1):
+	# Vamos a procesar la fila "r"
+	print "recorriendo", D[r]
+	for k in range(len(D[r])):
+		D[r][k] = D[r][k] + max(D[r+1][k] , D[r+1][k+1])
+
+print D[0][0]
