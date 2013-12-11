a7afaddbb462300774e19ef1813f1dae4bcf6206
proposicion47 <proposicion47@gmail.com>
2013-12-11 16:31:52 -0600

    adicion de conteo de divisores, optimizaciones usando memoria para reducir uso de cpu

--- pruebas.py
+++ pruebas.py
@@ -1,20 +1,3 @@
-import time
-def collatz(n):
-    if n<2: return 0
-    
-    if n%2 == 0 : 
-        return n/2
-    else:
-        return 3*n+1
+import primos
 
-def serie(m):
-    t=1
-    while m >1:
-        t=t+1
-        print m,
-        m = collatz(m)
-    print 
-    return t    
-
-
-print serie(7)
+print primos.ListaPrimos
