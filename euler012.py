2d6440c057854a4fe121bcecd73c1efac30d0786
proposicion47 <proposicion47@gmail.com>
2013-12-11 16:12:29 -0600

    migracion a libreria primos.py, 10x más veloz

--- euler012.py
+++ euler012.py
@@ -1,25 +1,5 @@
 import time
-import primos
-
-
-def cuentadivisores(n):
-	if n==1: return 1
-	if n<1: return 0
-
-
-	ndiv = 1
-	for p in primos.ListaPrimos:
-		c =1 
-		while n%p == 0:
-			c = c+1
-			n = n/p
-		ndiv = ndiv*c
-	
-		if n == 1: return ndiv
-		if p>n:
-			break
-	return ndiv
-
+from primos import ndivisores
 
 t=time.time()
 
@@ -29,9 +9,9 @@ n=2
 
 while True:
 	if n%2 != 0 :  # Si el actual es impar entonces el siguiente es par
-		siguiente = cuentadivisores( (n+1)/2)
+		siguiente = ndivisores( (n+1)/2)
 	else:
-		siguiente = cuentadivisores(n+1)
+		siguiente = ndivisores(n+1)
 	
 	numdivisores = actual*siguiente
 	if numdivisores>cota:
