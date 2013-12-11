4e3ca956c30d359859755f9ad3f3efe5d3514619
proposicion47 <proposicion47@gmail.com>
2013-12-10 18:08:09 -0600

    mayor suma en caminos de arreglo triangular

--- euler067.py
+++ euler067.py
@@ -1,5 +1,5 @@
-import os
-print os.getcwd()
+import time
+t = time.time()
 entrada = open("triangle.txt","r")
 D=[]
 for l in entrada:
@@ -9,8 +9,9 @@ nrows= len(D)
 
 for r in range(nrows-2,-1,-1):
 	# Vamos a procesar la fila "r"
-	print "recorriendo", D[r]
+	#print "recorriendo", D[r]
 	for k in range(len(D[r])):
 		D[r][k] = D[r][k] + max(D[r+1][k] , D[r+1][k+1])
 
 print D[0][0]
+print time.time()-t , "segundos"
