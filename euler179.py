a39b70119a088324e027ed0ca0c25e6405f5cea1
proposicion47 <proposicion47@gmail.com>
2013-12-11 16:23:52 -0600

    migracion a libreria primos.py, 10x más veloz

--- euler179.py
+++ euler179.py
@@ -1,31 +1,5 @@
 import time
-import primos
-
-
-def cuentadivisores(n):
-	if n==1: return 1
-	if n<1: return 0
-
-
-	ndiv = 1
-	for p in primos.ListaPrimos:
-		c =1 
-		while n%p == 0:
-			c = c+1
-			n = n/p
-		ndiv = ndiv*c
-	
-		if n == 1: return ndiv
-
-		if p*p>n:
-			# Si n>1 ya no tiene mas divisores...
-			# entonces n es primo y contribuye con p^1
-			ndiv = ndiv*2
-			break
-		if p>n:
-			break
-	return ndiv
-
+from primos import ndivisores
 
 t=time.time()
 
@@ -34,12 +8,10 @@ n=2
 conteo = 0
 for n in  xrange(2,10**7):
 	#if n%10000==0: print n,"..",
-	siguiente = cuentadivisores(n+1)
+	siguiente = ndivisores(n+1)
 	if actual ==siguiente: conteo = conteo+1
 	actual=siguiente
 print "\n======="
 print conteo
-print cuentadivisores(100)
-print cuentadivisores(88)
-print cuentadivisores(216)
 print time.time()-t
+
