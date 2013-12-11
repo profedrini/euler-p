50cc4188562156cfc38bedd728d2a2d5d78774ad
proposicion47 <proposicion47@gmail.com>
2013-12-11 17:36:58 -0600

    pruebas

--- pruebas.py
+++ pruebas.py
@@ -1,3 +1,25 @@
-import primos
+from aritmetica import abundante
+import time
 
-print primos.ListaPrimos
+t=time.time()
+top = 28124
+abundantes= filter(abundante,xrange(top))
+
+L = range(top)
+
+nab = len(abundantes)
+print "hay %d abundantes menores que %d " % (nab,top)
+
+for i in range(nab):
+	r = abundantes[i]
+	for j in range(i,nab):
+		s=abundantes[j]
+		#print (r,s)
+		if r+s<top:
+			if L[r+s]>0:
+				L[r+s]=0
+		else:
+			break
+
+print sum(L)
+print time.time()-t,"segundos"
