5ed44e17192b258bf6942d0ada74880cd9405371
proposicion47 <proposicion47@gmail.com>
2013-12-23 05:46:44 -0600

    test

--- pruebas.py
+++ pruebas.py
@@ -1,29 +1,14 @@
-#coding=utf8
-from aritmetica import *
-import time
-def cuentaprimos(a,b):
-	'''Cuenta cuántos primos consecutivos hay
-	al evaluar x^2 + ax + b   para x=0, 1, 2,...
-	'''
-	t = 0
-	x = 0
-	while True:
-		s = x*x + a*x + b
-		if s in ListaPrimos:
-			x = x+1
-			t = t+1
-		else:
-			return t
+from aritmetica import EsPrimo
+from itertools import permutations
+digits=map(str,range(1,10))
 
-t=time.time()
-T=1000
-mx = 2
-for b in range(T):
-	if b in ListaPrimos:
-		for a in range(-T,T):
-			cp=cuentaprimos(a,b)
-			if cp>mx:
-				print "(%d,%d) -> %d" % (a,b,cp)
-				mx = cp
 
-print time.time() -t 
+for k in range(9,7,-1):
+	stop=False
+	for p in permutations(digits,k):
+		n = int("".join(p))
+		if EsPrimo(n): 
+			print n
+			stop=True
+	if stop: break
+print digits
