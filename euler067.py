900748423f0b56ac3eade5eeb9479b763f403d5f
profedrini <profedrini@gmail.com>
2015-09-02 00:30:44 -0500

    Update euler067.py

--- euler067.py
+++ euler067.py
@@ -1,16 +1,15 @@
 import time
 t = time.time()
+
 entrada = open("triangle.txt","r")
 D=[]
 for l in entrada:
 	D.append(map(int,l.split()))
 entrada.close()
-
 nrows= len(D)
 
 for r in range(nrows-2,-1,-1):
-	# Vamos a procesar la fila "r"
-	#print "recorriendo", D[r]
+
 	for k in range(len(D[r])):
 		D[r][k] = D[r][k] + max(D[r+1][k] , D[r+1][k+1])
 
